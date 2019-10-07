6c43461bad92aeb91eda3f331c138a7c5e567479
mattstg <mattstger@gmail.com>
2019-10-07 13:07:26 -0400

    IsGrounded
    IsGrounded using the raycast

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public enum MoveType { Velo, Forces}
 
     Rigidbody2D rb;
     public float moveSpeed = 5;
     public float maxSpeed = 7;
-    public MoveType moveType = MoveType.Forces;
+    public float initialJumpForce = 10;
+
     public void FirstInitialize()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -17,21 +17,23 @@ public class Player : MonoBehaviour
 
     public void PhysicsRefresh(InputManager.InputPkg playerInput)
     {
-        switch (moveType) //sw tab tab varname enter enter
-        {
-            case MoveType.Velo:
-                rb.velocity = new Vector2(playerInput.dirPressed.x * moveSpeed, rb.velocity.y);
-                break;
-            case MoveType.Forces:
-                rb.AddForce(Vector2.right * playerInput.dirPressed * moveSpeed);
-                Vector2 velo = new Vector2(Mathf.Clamp(rb.velocity.x,-maxSpeed,maxSpeed),rb.velocity.y);
-                rb.velocity = velo;
-                break;
-            default:
-                Debug.LogError("Unhandled switch: " + moveType);
-                break;
-        }
-        
+        rb.AddForce(Vector2.right * playerInput.dirPressed * moveSpeed);
+        Vector2 velo = new Vector2(Mathf.Clamp(rb.velocity.x,-maxSpeed,maxSpeed),rb.velocity.y);
+        rb.velocity = velo;
+
+        Jump(playerInput.jumpPressed, playerInput.jumpHeld);
+
+    }
+    
+    private bool CheckIsGroundedRaycast()
+    {
+        return Physics2D.Raycast(transform.position, -transform.up, 1.1f, LayerMask.GetMask("Ground"));
+    }
+
+    private void Jump(bool jumpPressed, bool jumpHeld)
+    {
+        if(jumpPressed && CheckIsGroundedRaycast())
+            rb.AddForce(Vector2.up * initialJumpForce, ForceMode2D.Impulse);
     }
 
     public void Refresh(InputManager.InputPkg playerInput)
